b71d1197e94435a9eda0b04c137e2395d91ba349
Frank McSherry <fmcsherry@me.com>
2015-08-07 14:26:36 +0200

    better error information

--- src/typed_map.rs
+++ src/typed_map.rs
@@ -15,11 +15,11 @@ pub struct TypedMemoryMap<T:Copy> {
 
 impl<T:Copy> TypedMemoryMap<T> {
     pub fn new(filename: String) -> TypedMemoryMap<T> {
-        let file = File::open(filename).unwrap();
-        let size = file.metadata().unwrap().len() as usize;
+        let file = File::open(filename).ok().expect("error opening file");
+        let size = file.metadata().ok().expect("error reading metadata").len() as usize;
         TypedMemoryMap {
             map: mmap::MemoryMap::new(size, &[mmap::MapOption::MapReadable,
-                                              mmap::MapOption::MapFd(file.as_raw_fd())]).unwrap(),
+                                              mmap::MapOption::MapFd(file.as_raw_fd())]).ok().expect("error mapping memory"),
             len: size / mem::size_of::<T>(),
             phn: PhantomData,
         }
