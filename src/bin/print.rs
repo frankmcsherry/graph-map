952c32666d9415577e1f64c07492063ac986e3af
Frank McSherry <fmcsherry@me.com>
2017-05-27 13:35:07 -0400

    print bin

---
+++ src/bin/print.rs
@@ -0,0 +1,14 @@
+extern crate graph_map;
+use graph_map::GraphMMap;
+
+fn main() {
+    println!("usage: print <source>");
+
+    let filename = std::env::args().skip(1).next().unwrap();
+    let graph = GraphMMap::new(&filename);
+    for node in 0 .. graph.nodes() {
+        for &edge in graph.edges(node) {
+            println!("{}\t{}", node, edge);
+        }
+    }
+}
