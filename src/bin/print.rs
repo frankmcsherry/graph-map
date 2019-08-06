f2634f29a72cbf299854134f9d986c3abab32bbb
Frank McSherry <fmcsherry@me.com>
2019-08-06 10:06:20 -0400

    buffer writes

--- src/bin/print.rs
+++ src/bin/print.rs
@@ -6,10 +6,15 @@ fn main() {
 
     let filename = std::env::args().skip(1).next().unwrap();
     let graph = GraphMMap::new(&filename);
+
+    use std::io::Write;
+    let mut writer = std::io::BufWriter::new(std::io::stdout());
+
     for node in 0 .. graph.nodes() {
-        println!("degree[{}]: {}", node, graph.edges(node).len());
-        // for &edge in graph.edges(node) {
-        //     println!("{}\t{}", node, edge);
-        // }
+        for &edge in graph.edges(node) {
+            write!(&mut writer, "{}\t{}\n", node, edge).expect("Write failed");
+        }
     }
+
+    std::io::stdout().flush().expect("Flush failed");
 }
